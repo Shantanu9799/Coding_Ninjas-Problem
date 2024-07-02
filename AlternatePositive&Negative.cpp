0e563f8ad0d10bede5bd200880471bec2a4b380d
Shantanu Chatterjee <85756136+Shantanu9799@users.noreply.github.com>
2024-07-02 20:20:55 +0530

    Update AlternatePositive&Negative.cpp

--- AlternatePositive&Negative.cpp
+++ AlternatePositive&Negative.cpp
@@ -3,6 +3,31 @@
 void posAndNeg(vector<int> &arr)
 {
     // Write your code here.
+    int n = arr.size(), i = 0, j = 0;
+    vector<int> pos(n/2), neg(n/2);
+    for(auto it : arr) {
+        if(it >= 0) pos[i++] = it;
+        else neg[j++] = it;
+    }
+    i = 0; j = 0;
+    for(int idx = 0; idx < n; ++idx) {
+        if((idx & 1) == 0) {
+            arr[idx] = pos[i++];
+        } else {
+            arr[idx] = neg[j++];
+        }
+    }
+}
+
+
+**********************************************
+
+
+#include <bits/stdc++.h> 
+
+void posAndNeg(vector<int> &arr)
+{
+    // Write your code here.
     vector<int> pos;
     vector<int> neg;
     for(int i : arr) {
